9a561904b254192093a9c259e0323f993da069c0
Viviana Garcia <vivianandrea4@gmail.com>
2020-11-25 12:59:49 -0500

    footer changes

--- src/landing/form/form.js
+++ src/landing/form/form.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './form.css'
+import guitar from './../../assets/LandingPics/guitar.svg'
+import wallet from './../../assets/LandingPics/wallet.svg'
+import checkmark from './../../assets/LandingPics/authenticity-seal.svg'
 
 const Form = () => {
     return (
         <div className="form">
             <div className="guitar">
-                <img className="images" src="./landingpics/guitar.svg" alt="guitar photo" />
+                <img className="images" src={guitar} alt="guitar photo" />
                 <h5 className="form-text">The only place to get the exclusive Jimi Hendrix "Experience 27" collection</h5>
             </div>
             <div className="wallet">
-                <img className="images" src="./landingpics/wallet.svg" alt="wallet photo" />
+                <img className="images" src={wallet} alt="wallet photo" />
                 <h5 className="form-text">Make payments easier to afford. Now accepting AfterPay and Klarna payment installments.</h5>
             </div>
             <div className="checkmark">
-                <img className="images" src="./landingpics/authenticity-seal.svg" alt="authenticity photo" />
+                <img className="images" src={checkmark} alt="authenticity photo" />
                 <h5 className="form-text">Know your products are authentic Jimi Hendrix items with our Authenticity Seals.</h5>
             </div>
         </div>
